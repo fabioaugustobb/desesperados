25678547f52d6b0aa4b6fdb751aa17162c9fb84d
a1320742 <a1320742@192.168.1.77>
2015-08-14 21:42:12 -0300

    Criação da Classe Jogador

---
+++ Campeonado_Desesperados_POO/src/campeonado_desesperados_poo/Jogador.java
@@ -0,0 +1,29 @@
+/*
+    Classe jogador recebe os parÃ¢metros da tela de seleção.
+ */
+package campeonado_desesperados_poo;
+
+/*
+ * Mariana Matias 
+ */
+public class Jogador {
+    
+    // Declaração das variáveis
+    private int    Idjogador;
+    private String Nome;
+    private int    Rg;
+    private String Endereco;
+    private int    Telefone;
+    
+    public Jogador(){
+        // Inicializar as variáveis
+        Idjogador = 0;
+        Nome      = "";
+        Rg        = 0;
+        Endereco  = "";
+        Telefone  = 0;        
+        
+    }
+    //Leitura do Nome do Jogador
+    
+}
